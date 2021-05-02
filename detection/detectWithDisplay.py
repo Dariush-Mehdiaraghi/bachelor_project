8b1d202515d9f1e85a8a1dc79d4bb79db67416bd
Dariush <dariush007@gmx.ch>
2021-05-02 12:57:43 +0200

    first commit

---
+++ detection/detectWithDisplay.py
@@ -0,0 +1,192 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A demo that runs object detection on camera frames using OpenCV.
+
+TEST_DATA=../all_models
+
+Run face detection model:
+python3 detect.py \
+  --model ${TEST_DATA}/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite
+
+Run coco model:
+python3 detect.py \
+  --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
+  --labels ${TEST_DATA}/coco_labels.txt
+
+"""
+import argparse
+import cv2
+import os
+
+from pycoral.adapters.common import input_size
+from pycoral.adapters.detect import get_objects
+from pycoral.utils.dataset import read_label_file
+from pycoral.utils.edgetpu import make_interpreter
+from pycoral.utils.edgetpu import run_inference
+
+#Imports for display
+import digitalio
+import board
+from PIL import Image, ImageDraw
+import adafruit_rgb_display.ili9341 as ili9341
+import adafruit_rgb_display.st7789 as st7789  # pylint: disable=unused-import
+import adafruit_rgb_display.hx8357 as hx8357  # pylint: disable=unused-import
+import adafruit_rgb_display.st7735 as st7735  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1351 as ssd1351  # pylint: disable=unused-import
+import adafruit_rgb_display.ssd1331 as ssd1331  # pylint: disable=unused-import
+ 
+
+
+def main():
+        # Configuration for CS and DC pins (these are PiTFT defaults):
+    cs_pin = digitalio.DigitalInOut(board.CE0)
+    dc_pin = digitalio.DigitalInOut(board.D25)
+    reset_pin = digitalio.DigitalInOut(board.D24)
+    
+    # Config for display baudrate (default max is 24mhz):
+    BAUDRATE = 24000000
+    
+    # Setup SPI bus using hardware SPI:
+    spi = board.SPI()
+
+    # pylint: disable=line-too-long
+    # Create the display:
+    # disp = st7789.ST7789(spi, rotation=90,                            # 2.0" ST7789
+    disp = st7789.ST7789(spi, height=240, y_offset=80, rotation=180,  # 1.3", 1.54" ST7789
+    # disp = st7789.ST7789(spi, rotation=90, width=135, height=240, x_offset=53, y_offset=40, # 1.14" ST7789
+    # disp = hx8357.HX8357(spi, rotation=180,                           # 3.5" HX8357
+    # disp = st7735.ST7735R(spi, rotation=90,                           # 1.8" ST7735R
+    # disp = st7735.ST7735R(spi, rotation=270, height=128, x_offset=2, y_offset=3,   # 1.44" ST7735R
+    # disp = st7735.ST7735R(spi, rotation=90, bgr=True,                 # 0.96" MiniTFT ST7735R
+    # disp = ssd1351.SSD1351(spi, rotation=180,                         # 1.5" SSD1351
+    # disp = ssd1351.SSD1351(spi, height=96, y_offset=32, rotation=180, # 1.27" SSD1351
+    # disp = ssd1331.SSD1331(spi, rotation=180,                         # 0.96" SSD1331
+    #disp = ili9341.ILI9341(
+    #    spi,
+    #    rotation=90,  # 2.2", 2.4", 2.8", 3.2" ILI9341
+        cs=cs_pin,
+        dc=dc_pin,
+        rst=reset_pin,
+        baudrate=BAUDRATE,
+    )
+    # Create blank image for drawing.
+    # Make sure to create image with mode 'RGB' for full color.
+    if disp.rotation % 180 == 90:
+        height = disp.width  # we swap height/width to rotate it to landscape!
+        width = disp.height
+    else:
+        width = disp.width  # we swap height/width to rotate it to landscape!
+        height = disp.height
+
+    image = Image.new("RGB", (width, height))
+    
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
+    
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+    disp.image(image)
+    
+    default_model_dir = '../all_models'
+    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+    default_labels = 'coco_labels.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=3,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
+    parser.add_argument('--threshold', type=float, default=0.1,
+                        help='classifier score threshold')
+    args = parser.parse_args()
+
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
+    labels = read_label_file(args.labels)
+    inference_size = input_size(interpreter)
+
+    cap = cv2.VideoCapture(args.camera_idx)
+    
+    if cap.isOpened():
+        ret, frame = cap.read()
+        cv2_im = frame
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+        image = Image.fromarray(cv2_im)
+        image_ratio = image.width / image.height
+        screen_ratio = width / height
+        # Scale the image to the smaller screen dimension
+        if screen_ratio < image_ratio:
+            scaled_width = image.width * height // image.height
+            scaled_height = height
+        else:
+            scaled_width = width
+            scaled_height = image.height * width // image.width
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2_im = frame
+
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+        run_inference(interpreter, cv2_im_rgb.tobytes())
+        objs = get_objects(interpreter, args.threshold)[:args.top_k]
+        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
+
+        image = Image.fromarray(cv2_im)
+
+        image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+        
+        # Crop and center the image
+        x = scaled_width // 2 - width // 2
+        y = scaled_height // 2 - height // 2
+        image = image.crop((x, y, x + width, y + height))
+
+        disp.image(image)
+
+
+
+        cv2.imshow('frame', cv2_im)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def append_objs_to_img(cv2_im, inference_size, objs, labels):
+    height, width, channels = cv2_im.shape
+    scale_x, scale_y = width / inference_size[0], height / inference_size[1]
+    for obj in objs:
+        bbox = obj.bbox.scale(scale_x, scale_y)
+        x0, y0 = int(bbox.xmin), int(bbox.ymin)
+        x1, y1 = int(bbox.xmax), int(bbox.ymax)
+        position= ((x0+x1)/2) / 640 #image_width
+        percent = int(100 * obj.score)
+        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+        
+        foundObject = {"label": obj.id, "position": position}   
+        print(foundObject)
+        os.system("echo '" + str(obj.id)  + " " + str(position) + ";" + "' | pdsend 3000")
+        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+                             cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+    return cv2_im
+
+if __name__ == '__main__':
+    main()
---
+++ testScriptForPD.py
@@ -0,0 +1,5 @@
+import os
+os.system("echo '" + str(8)  + " " + str(0.54231) + ";" + "' | pdsend 3000")
+os.system("echo '" + str(3)  + " " + str(0.312) + ";" + "' | pdsend 3000")
+os.system("echo '" + str(9)  + " " + str(0.22) + ";" + "' | pdsend 3000")
+os.system("echo '" + str(1)  + " " + str(0.222) + ";" + "' | pdsend 3000")
